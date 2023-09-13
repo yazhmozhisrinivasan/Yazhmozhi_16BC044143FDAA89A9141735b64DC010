ddf30e70e599a0546be4070f4ac35606a70f034e
yazhmozhisrinivasan <144765046+yazhmozhisrinivasan@users.noreply.github.com>
2023-09-13 15:14:02 +0530

    player.py

---
+++ player.py
@@ -0,0 +1,19 @@
+class player:
+    def play(self):
+        print("the player is playing cricket.")
+
+class batman(player):
+    def play(self):
+        print("the batman is batting.") 
+
+class bowler(player):
+    def play(self):
+        print("the bowler is bowling.")
+# create objects of both batman and bowler classes and call the play() method for each object.
+batsman = btsman()
+bowler = bowler()
+
+batsman.play()  # this will
+print "the batsman is batting."
+bowler.play()   # this will
+print "the bowler is bowling."
